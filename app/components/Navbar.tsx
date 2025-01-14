1a846aa796525c214c38d9142910a561ca519b4f
Ash-Har-Ali <20gcs53@meaec.edu.in>
2025-01-14 16:05:27 +0530

    Header

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,23 +21,24 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 my-4 md:mx-24 mx-4 border border-white border-opacity-10 bg-[#f2f2f2] rounded-[3rem] px-4 py-2">
+    <nav className="fixed top-0 left-0 right-0 z-50 my-4 md:mx-24 mx-4 border border-white border-opacity-30 bg-[#f2f2f2] rounded-[3rem] px-4 py-2">
       <div className="flex container items-center justify-between mx-auto px-4 py-2">
         {/* Left Side: Logo */}
         <Link href="/" className="text-2xl md:text-3xl text-black font-semibold">
-          <Image src={Logo} alt="WizzyMinds" className="h-10 w-auto"/>
+          <Image src={Logo} alt="Solar Cool" className="h-16 w-auto" />
         </Link>
 
         {/* Center: Navigation Links (hidden on mobile) */}
-        <div className="hidden xl:flex space-x-8">
+        <div className="hidden xl:flex space-x-8 justify-center flex-grow">
           {navLinks.map((link, index) => (
             <NavLink key={index} href={link.path} title={link.title} />
           ))}
         </div>
+
         <div className="flex items-center">
           {/* Right Side: CTA Button (hidden on mobile) */}
           <div className="hidden lg:block">
-          <Link
+            <Link
               href="/contact"
               className="px-4 inline-block py-4 w-full sm:w-fit rounded-full mr-4 border border-white border-opacity-40 bg-[#036d39] text-white hover:bg-gray-800 transition text-sm sm:text-base"
             >
@@ -66,9 +67,9 @@ const Navbar = () => {
               className="text-white focus:outline-none"
             >
               {!navbarOpen ? (
-                <Bars3Icon className="h-6 w-6" />
+                <Bars3Icon className="h-6 w-6 text-[#036d39]" />
               ) : (
-                <XMarkIcon className="h-6 w-6" />
+                <XMarkIcon className="h-6 w-6 text-[#036d39]" />
               )}
             </button>
           </div>
