735fc142d6251628eb12a7bc4ed0e0711acb917d
Ash-Har-Ali <20gcs53@meaec.edu.in>
2025-02-19 15:17:59 +0530

    Configure for Render Web Service deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   images: {
     domains: ["cdn.sanity.io"],
     unoptimized: true,
-  },
-  webpack: (config) => {
-    config.watchOptions = {
-      aggregateTimeout: 300,
-      poll: 1000,
-    };
-    return config;
   },
   poweredByHeader: false,
   reactStrictMode: true,
