213de901a1db8f60fb0eadd612f584ec1059a182
Ash-Har-Ali <20gcs53@meaec.edu.in>
2025-02-22 11:02:55 +0530

    First commit v1

--- app/products/ac/page.tsx
+++ app/products/ac/page.tsx
@@ -1,7 +1,7 @@
 import ProductPage from "../components/ProductPage";
 
 const AcPage: React.FC = () => {
-  return <ProductPage category="television" bannerImage="/images/banner-products.webp" />;
+  return <ProductPage category="airconditioner" bannerImage="/images/banner-products.webp" />;
 };
 
 export default AcPage;
